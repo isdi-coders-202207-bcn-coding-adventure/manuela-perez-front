6e9211ce7b23c1b9343fdc071ddbc6f1252ebea2
Manuela Perez Simon <manuelaperezsimon@gmail.com>
2022-08-22 15:16:47 +0200

    Created the html skeleton of counter component

---
+++ src/components/Counter/Counter.tsx
@@ -0,0 +1,14 @@
+const Counter = (): JSX.Element => {
+  return (
+    <>
+      <section className="counter-container">
+        <span className="counter-days">02</span>
+        <span className="counter-hours">20</span>
+        <span className="counter-minutes">34</span>
+        <span className="counter-seconds">09</span>
+      </section>
+    </>
+  );
+};
+
+export default Counter;
